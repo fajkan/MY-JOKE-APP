6f2666b57606527401a08cb090e551091d5c124a
Armin <fajkan10@gmail.com>
2021-05-24 09:32:22 +0200

    Initial commit

---
+++ .idea/App.js
@@ -0,0 +1,13 @@
+import Joke from "./components/Joke";
+
+function App(){
+    return (
+        <div className="App">
+            <Joke>
+
+            </Joke>
+        </div>
+    )
+}
+
+export default App;
